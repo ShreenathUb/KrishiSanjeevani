fa63c8313a9e40219b220eb6f2229658c1b1b11c
ShreenathUB <shriubale06@gmail.com>
2025-07-11 20:12:25 +0530

    made necessary changes for vite url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,12 +1,15 @@
 // frontend/src/utils/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
-
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const api = axios.create({
   baseURL: API_BASE_URL,
-  headers: { 'Content-Type': 'application/json' },
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
+
 
 /* ─────────────────────────────────────────  FARMER  ─── */
 export const farmerAPI = {
